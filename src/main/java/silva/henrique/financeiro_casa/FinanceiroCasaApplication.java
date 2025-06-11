6bbd52e84dcb551e518c22c24ee2d9dfc2415cfb
Henrique Silva <henriquersilva.al@gmail.com>
2025-06-11 00:05:49 -0300

    initial commit

---
+++ src/main/java/silva/henrique/financeiro_casa/FinanceiroCasaApplication.java
@@ -0,0 +1,13 @@
+package silva.henrique.financeiro_casa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroCasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroCasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/silva/henrique/financeiro_casa/FinanceiroCasaApplicationTests.java
@@ -0,0 +1,13 @@
+package silva.henrique.financeiro_casa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceiroCasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
